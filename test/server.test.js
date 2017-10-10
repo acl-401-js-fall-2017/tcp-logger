aa883b38e1e7606ab961391a4492fb585e47038b
ckrountree <jmail0207@gmail.com>
2017-10-10 13:04:18 -0700

    write and pass two messages in log.txt

---
+++ test/server.test.js
@@ -0,0 +1,52 @@
+const assert = require('assert');
+const createLogServer = require('../lib/create-log-server');
+const net = require('net');
+const fs = require('fs');
+const path = require('path');
+
+describe('chat app server', () => {
+    const port = 15688;
+    const logFile = path.join(__dirname, 'log.txt');
+
+    beforeEach(done => {
+        fs.unlink(logFile, err => {
+            if(err && err.code !== 'ENOENT') done(err);
+            done();
+        });
+    });
+    
+    let clients = null;
+    let server = null;
+    beforeEach(done => {
+        clients = [];
+        server = createLogServer(logFile);
+        server.listen(port, done);
+    });
+    
+    afterEach(() => {
+        clients.forEach(client => client.destroy());
+        server.close();
+    });
+
+    function openClient(callback) {
+        let client = net.connect(port, () => {
+            client.setEncoding('utf8');
+            clients.push(client);
+            callback(null, client);
+        });
+    }
+
+    it('runs a test', done => {
+        openClient((err, client1) => {
+            client1.write('Client One');
+            openClient((err, client2) => {
+                client2.write('Client Two', () => {
+                    done();
+                });
+                
+            });
+        });
+    });
+
+
+});
