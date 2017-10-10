8611cdf53ce1c42dfab14b0265bc23d68764724f
David Nishikawa <triptype@gmail.com>
2017-10-10 13:13:36 -0700

    make server write messages to log file and input correct dates and messages

--- test/server.test.js
+++ test/server.test.js
@@ -3,6 +3,7 @@ const createLogServer = require('../lib/create-log-server');
 const net = require('net');
 const fs = require('fs');
 const path = require('path');
+const parseLog = require('../lib/parse-log');
 
 describe('chat app server', () => {
     const port = 15688;
@@ -38,16 +39,45 @@ describe('chat app server', () => {
 
     it('runs a test', done => {
         openClient((err, client1) => {
-            openClient((err, client2) => {
-                // do client.write calls
-                // on last client.write call, you need to use the
-                // write callback to *wait" for the socket to finish before you test the log file
-                client2.write('some message', () => {
-                    // read log file and test here!
+            client1.write('this is a message', () => {
+                openClient((err, client2) => {
+                    // do client.write calls
+                    // on last client.write call, you need to use the
+                    // write callback to *wait" for the socket to finish before you test the log file
+                    client1.write('second message from cli1');
                     
-                    done();
+                    client2.write('some message', () => {
+                        // read log file and test here!
+                        setTimeout(() => {
+                            let logMessages = [];
+                            let logDates = [];
+                            parseLog(logFile, logData => {
+
+                                logMessages = logData.map(lineObj => lineObj.message);
+                                logDates = logData.map(lineObj => lineObj.time);
+                                
+                                assert.deepEqual(
+                                    logMessages,
+                                    [
+                                        'new client connected',
+                                        'this is a message',
+                                        'new client connected',
+                                        'second message from cli1',
+                                        'some message'
+                                    ]
+                                );
+
+                                logDates.forEach(date => {
+                                    assert.ok(!isNaN(new Date(date).getTime()));
+                                });
+
+                                done();
+                            });
+                        }, 500);
+                    });
+                    
                 });
-                
+
             });
         });
     });
